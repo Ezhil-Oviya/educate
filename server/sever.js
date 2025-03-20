d94b4063d9fbf23aa72fc3f71ae5db4481dc4117
Ezhil Oviya <ezhiloviya@gmail.com>
2025-03-20 22:22:27 +0530

    Updated  server logic;

--- server/sever.js
+++ server/sever.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const mongoose=require('mongoose');
 const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON
+app.use(express.json()); 
 
 connectDB();
 
@@ -12,4 +13,10 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI=process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
