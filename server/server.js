7d31cee74e1129354d03071cbd0c3662b5cf7ba1
Ezhil Oviya <ezhiloviya@gmail.com>
2025-03-20 22:24:22 +0530

    Updated  server logic;

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const app = express();
+const mongoose=require('mongoose');
+const connectDB = require("./config/db");
+require("dotenv").config();
 
-const PORT = 3000;
+const app = express();
+app.use(express.json()); 
+
+connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Server is running!");
+  res.send("API Running...");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+const MONGO_URI=process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB Connection Error:", err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
